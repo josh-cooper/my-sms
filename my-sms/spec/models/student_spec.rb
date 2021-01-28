d4783bf8c22e05bdcebeb5827d5ef9317556e9d7
Josh Cooper <cooperj3@utas.edu.au>
2021-01-28 23:55:49 +0000

    Add student model specs

--- my-sms/spec/models/student_spec.rb
+++ my-sms/spec/models/student_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Student, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:student)).to be_valid
+  end
+
+  it 'has a factory that generates unique emails' do
+    student = FactoryBot.build(:student)
+    other_student = FactoryBot.build(:student)
+    expect(student.email).not_to eq other_student.email
+  end
+
+  it 'is invalid without a first name' do
+    user = FactoryBot.build(:student, first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include('can\'t be blank')
+  end
 end
