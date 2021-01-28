7798e44ca16d2a2462039d47c84f0641b7220d06
Josh Cooper <cooperj3@utas.edu.au>
2021-01-28 04:40:19 +0000

    Add Student validations

--- my-sms/app/models/student.rb
+++ my-sms/app/models/student.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class Student < ActiveRecord::Base
+  attr_accessible :title, :first_name, :middle_name, :last_name, :email,
+                  :birth_date, :gender
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP,
+                              message: 'Must be a valid email' }
+  validates :birth_date, presence: true
 end
