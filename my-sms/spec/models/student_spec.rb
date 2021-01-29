fe1d0d8ca8dd4f6a9401a31dc202e1818886e041
Josh Cooper <cooperj3@utas.edu.au>
2021-01-29 02:20:34 +0000

    Add additional student models specs

--- my-sms/spec/models/student_spec.rb
+++ my-sms/spec/models/student_spec.rb
@@ -13,9 +13,26 @@ RSpec.describe Student, type: :model do
     expect(student.email).not_to eq other_student.email
   end
 
+  it 'is invalid with a duplicate email address' do
+    FactoryBot.create(:student, email: 'user@example.com')
+    student = FactoryBot.build(:student, email: 'user@example.com')
+    student.valid?
+    expect(student.errors[:email]).to include('has already been taken')
+  end
+
   it 'is invalid without a first name' do
-    user = FactoryBot.build(:student, first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include('can\'t be blank')
+    student = FactoryBot.build(:student, first_name: nil)
+    student.valid?
+    expect(student.errors[:first_name]).to include('can\'t be blank')
+  end
+
+  it 'is invalid without a last name' do
+    student = FactoryBot.build(:student, last_name: nil)
+    student.valid?
+    expect(student.errors[:last_name]).to include('can\'t be blank')
+  end
+
+  it 'is valid without a gender' do
+    expect(FactoryBot.build(:student, gender: nil)).to be_valid
   end
 end
