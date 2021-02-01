349dd0a5a4547203c5a5e487627f376b09487ca3
Josh Cooper <cooperj3@utas.edu.au>
2021-02-01 16:29:24 +1100

    Add pagination to students listing
     - Add will_paginate dependency
     - Change rendering all posts in students index to a paginated query
     - Render page buttons in students index erb
     - Set per page limit to 10

--- my-sms/app/controllers/students_controller.rb
+++ my-sms/app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   # GET /students
   # GET /students.json
   def index
-    @students = Student.all
+    @students = Student.paginate(page: params[:page], per_page: 30)
 
     respond_to do |format|
       format.html # index.html.erb
