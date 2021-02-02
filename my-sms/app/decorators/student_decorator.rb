f070817eabf0bc4f6fc9ddecb6fcf48e1cc909a7
Josh Cooper <cooperj3@utas.edu.au>
2021-02-02 23:10:30 +0000

    Clean up full_name method.
    Replace string interpolation with array join.

--- my-sms/app/decorators/student_decorator.rb
+++ my-sms/app/decorators/student_decorator.rb
@@ -4,9 +4,6 @@ class StudentDecorator < Draper::Decorator
   delegate_all
 
   def full_name
-    "#{title + ' ' if title.present?}"\
-    "#{first_name} "\
-    "#{middle_name + ' ' if middle_name.present?}"\
-    "#{last_name}"
+    [title, first_name, middle_name, last_name].select(&:present?).join(' ')
   end
 end
