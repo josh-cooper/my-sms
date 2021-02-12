453cd248af4c2a2ecca026d7bcae849771f6fa83
Josh Cooper <cooperj3@utas.edu.au>
2021-02-12 04:22:18 +0000

    Refactor title migration
     - Unrecognised old titles in the students table are added to the titles table as part of the migration
     - If there are unrecognised titles in the students table warn instead of refusing to migrate
     - Run migration as two steps:
         - Create and populate titles table
         - Change Student title column from str to Title model
     - Find invalid titles via query interface
     - Access model properties via hash interface

---
+++ my-sms/db/migrate/20210212025754_move_student_title_str_to_title_model.rb
@@ -0,0 +1,44 @@
+class MoveStudentTitleStrToTitleModel < ActiveRecord::Migration
+  def up
+    # cache titles from db to avoid multiple queries
+    title_name_to_id = {}
+    Title.all.each do |title|
+      title_name_to_id[title.name] = title.id
+    end
+
+    # update students table
+    change_table :students do |t|
+      t.rename(:title, :title_old)
+      t.references :title
+    end
+
+    # add title model association
+    # without making assumptions about model
+    # add any titles not already in the db
+    Student.reset_column_information
+    Student.find_each do |student|
+      title = Title.where(name: student[:title_old]).first_or_create
+      student.update_attribute(:title_id, title.id)
+      student.save
+    end
+
+    # remove old title (string) column
+    remove_column :students, :title_old
+  end
+
+  def down
+    # re-create string title column
+    change_table :students do |t|
+      t.string :title_str
+    end
+
+    # undo title model association without assuming title implementation in model
+    Student.find_each do |student|
+      student.update_attribute(:title_str, Title.find(student[:title_id]).name)
+    end
+
+    # remove old title (model) column and rename title_str to title
+    remove_column :students, :title_id
+    rename_column :students, :title_str, :title
+  end
+end
--- my-sms/db/schema.rb
+++ my-sms/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20210208041633) do
+ActiveRecord::Schema.define(:version => 20210212025754) do
 
   create_table "students", :force => true do |t|
     t.string   "first_name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(:version => 20210208041633) do
   end
 
   create_table "titles", :force => true do |t|
-    t.string   "name"
+    t.string   "name",       :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
