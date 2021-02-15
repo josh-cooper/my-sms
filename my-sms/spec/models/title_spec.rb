9c1858a113f78d27f678467ebe5a7b6fb3efd5ed
Josh Cooper <cooperj3@utas.edu.au>
2021-02-15 22:56:27 +0000

    Refactor title spec
     - Replace before filter with let bang method
     - Use expect syntax over should syntax

--- my-sms/spec/models/title_spec.rb
+++ my-sms/spec/models/title_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe Title, type: :model do
 
   context 'a title with the same name exists' do
     let(:title) { build(:title, name: 'honorific') }
-    let(:other_title) { create(:title, name: 'honorific') }
+    let!(:other_title) { create(:title, name: 'honorific') }
 
     before { other_title }
 
     it 'is is invalid with a duplicate name' do
-      should be_invalid
+      is_expected.to be_invalid
     end
   end
 end
