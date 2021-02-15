fa498c484ce73c961075a7bcca20559da49e0d08
Josh Cooper <cooperj3@utas.edu.au>
2021-02-15 03:29:44 +0000

    Add title model specs

---
+++ my-sms/spec/models/title_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Title, type: :model do
+  subject { title }
+  let(:title) { build(:title, title_params) }
+  let(:title_params) { {} }
+
+  context 'with valid params' do
+    it { should validate_presence_of(:name) }
+  end
+
+  context 'a title with the same name exists' do
+    let(:title) { build(:title, name: 'honorific') }
+    let(:other_title) { create(:title, name: 'honorific') }
+
+    before { other_title }
+
+    it 'is is invalid with a duplicate name' do
+      should be_invalid
+    end
+  end
+end
