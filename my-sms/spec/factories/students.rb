caed22a39dd60ef6306182bb45af87460a936a45
Josh Cooper <41467557+josh-cooper@users.noreply.github.com>
2021-02-17 09:24:26 +1100

    Remove in-file faker import
    - Move calls to Faker into a block

--- my-sms/spec/factories/students.rb
+++ my-sms/spec/factories/students.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'faker'
-
 FactoryBot.define do
   factory :student do
     association :title, strategy: :build
@@ -14,11 +12,11 @@ FactoryBot.define do
   end
   factory :student_demo, class: :student do
     title_id { [nil, *Title.all.map(&:id)].sample }
-    first_name Faker::Name.first_name
+    first_name { Faker::Name.first_name }
     middle_name { [nil, Faker::Name.middle_name].sample }
-    last_name Faker::Name.last_name
-    email Faker::Internet.unique.email
-    birth_date Faker::Date.birthday(min_age: 18, max_age: 65)
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.unique.email }
+    birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
     gender { [nil, Faker::Gender.type].sample }
   end
 end
