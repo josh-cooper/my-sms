8aebed6989a77f9c68e092ce3496aea539d0361f
Josh Cooper <cooperj3@utas.edu.au>
2021-02-18 21:40:51 +0000

    Migrate institute model

--- my-sms/db/schema.rb
+++ my-sms/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20210216172154) do
+ActiveRecord::Schema.define(:version => 20210218213737) do
 
   create_table "courses", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(:version => 20210216172154) do
     t.integer  "allocation"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
+  end
+
+  create_table "institutes", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "students", :force => true do |t|
