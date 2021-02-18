5f3c7ddcae8e0aea443eb3eb0ab75e6ccb8868f0
Josh Cooper <cooperj3@utas.edu.au>
2021-02-18 07:04:05 +0000

    Refactor date presence check in end_date_after_start validator

--- my-sms/app/models/course.rb
+++ my-sms/app/models/course.rb
@@ -11,7 +11,7 @@ class Course < ActiveRecord::Base
   validate :end_date_after_start
 
   def end_date_after_start
-    return unless !end_date.blank? && !start_date.blank?
+    return if end_date.blank? || start_date.blank?
 
     unless end_date >= start_date
       errors.add(:end_date, 'must be after start date')
