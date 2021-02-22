d504394bc0fac857978f4214f585bfd2a9ce3ac0
Josh Cooper <cooperj3@utas.edu.au>
2021-02-22 01:09:17 +0000

    Update courses and institutes factories to reflect association

--- my-sms/spec/factories/courses.rb
+++ my-sms/spec/factories/courses.rb
@@ -10,13 +10,14 @@ FactoryBot.define do
     number_of_semesters { rand(1..3) }
     start_date 3.years.ago.to_date
     end_date 2.years.ago.to_date
-  end
-  factory :course_demo, class: :course do
-    name { Faker::Educator.course_name }
-    description { Faker::Lorem.paragraph }
-    allocation { rand(1..50) }
-    number_of_semesters { rand(1..3) }
-    start_date Faker::Date.between(from: '2014-01-1', to: '2014-06-15')
-    end_date Faker::Date.between(from: '2014-06-16', to: '2014-12-30')
+    institute
+
+    factory :course_demo, class: :course do
+      name { Faker::Educator.course_name }
+      description { Faker::Lorem.paragraph }
+      start_date Faker::Date.between(from: '2014-01-1', to: '2014-06-15')
+      end_date Faker::Date.between(from: '2014-06-16', to: '2014-12-30')
+      institute { association :institute_demo }
+    end
   end
 end
