cb06b09f0dd87be98de00cf28dce789da850fe1b
Josh Cooper <joshua.cooper@readytech.io>
2021-02-23 05:54:50 +0000

    Test factory validity

--- my-sms/spec/models/institute_spec.rb
+++ my-sms/spec/models/institute_spec.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Institute, :type => :model do
+RSpec.describe Institute, type: :model do
   subject { institute }
   let(:institute) { build(:institute, institute_params) }
   let(:institute_params) { {} }
 
   context 'with valid params' do
+    it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
   end
 end
