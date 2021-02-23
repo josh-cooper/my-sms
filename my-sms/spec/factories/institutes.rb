52bc9b0b2a26ce81550b50e67027df0bb279029b
Josh Cooper <joshua.cooper@readytech.io>
2021-02-23 05:54:36 +0000

    Refactor institute demo factory into a trait

--- my-sms/spec/factories/institutes.rb
+++ my-sms/spec/factories/institutes.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
   factory :institute do
     sequence(:name) { |n| "Institute #{n}" }
   end
-  factory :institute_demo, class: :institute do
+  trait :demo do
     name { [Faker::Educator.university, Faker::Educator.secondary_school].sample }
   end
 end
